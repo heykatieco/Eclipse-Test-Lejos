2c506cb043345c899c564632e07f269697c077fd
Katie King <73963685+heykatieco@users.noreply.github.com>
2022-10-26 16:14:43 -0500

    Add files via upload

---
+++ World.java
@@ -0,0 +1,12 @@
+package hello;
+
+import lejos.utility.Delay; 
+
+public class World {
+	
+	public static void main(String[] args) {
+		System.out.println("Testing screen");
+		Delay.msDelay(5000);
+	}
+
+}
